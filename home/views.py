dd54fbd678272510365c3ef8a3d404a8c127946f
Crowdbotics <engineering@crowdbotics.com>
2022-11-01 01:36:23 +0000

    add package blaze

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'blaze', 'url': 'http://pypi.python.org/pypi/blaze/0.10.1'},
 	{'name':'BlazeUtils', 'url': 'http://pypi.python.org/pypi/BlazeUtils/0.5.3'},
 	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
 	{'name':'django-bootstrap4', 'url': 'https://pypi.org/project/django-bootstrap4/0.0.7/'},
